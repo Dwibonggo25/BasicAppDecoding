21d4b41263d63dbfdbd0cade094540369f3c475d
Dwi Bonggo <dwibonggo25@gmail.com>
2019-10-10 17:21:30 +0700

    Add anko

--- app/src/main/java/com/example/basickotlindecoding/MainActivity.kt
+++ app/src/main/java/com/example/basickotlindecoding/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.example.basickotlindecoding
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import org.jetbrains.anko.*
+import org.jetbrains.anko.design.snackbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MainActivityUi().setContentView(this)
+    }
+
+    class MainActivityUi : AnkoComponent <MainActivity> {
+        override fun createView(ui: AnkoContext<MainActivity>)  = with(ui) {
+
+            verticalLayout{
+
+                padding = dip (16)
+
+                val name = editText() {
+                    hint = "What's your name ?"
+                }
+
+                button ("Say Hello"){
+                    textColor = Color.WHITE
+                    setOnClickListener {
+                        snackbar("Hello, ${name.text}!")
+                    }
+                }.lparams(width = matchParent){
+                    topMargin = dip(5)
+                }
+
+                button("Show Alert") {
+
+                }
+            }
+        }
+
     }
 }
